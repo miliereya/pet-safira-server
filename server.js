ba320238c78dea1fb50790065ad2decb6b298ddc
miliereya <105806526+miliereya@users.noreply.github.com>
2022-10-07 13:56:01 +0300

    1.2

--- server.js
+++ server.js
@@ -23,14 +23,6 @@ app.use('/public', express.static('public'))
 app.use(errorMiddleware)
 
 // Serve static assets if in production
-if(process.env.NODE_ENV === 'production'){
-    //Set static folder
-    app.use(express.static('client/build'))
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
 
 const uri = 'mongodb+srv://test:test@cluster0.uekb3.mongodb.net/?retryWrites=true&w=majority'
 
